9fafd0d7771b528a245137653a06ceb40808d58e
Juan V <valentin.juan05@gmail.com>
2023-07-24 23:12:28 -0300

    Completed CRUD payments service

---
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -0,0 +1,54 @@
+package br.com.alurafood.pagamentos.service;
+
+import br.com.alurafood.pagamentos.dto.PagamentoDTO;
+import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.model.Status;
+import br.com.alurafood.pagamentos.repository.PagamentoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PagamentoService {
+
+    @Autowired
+    private PagamentoRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<PagamentoDTO> obterTodos(Pageable paginacao){
+        return repository
+                .findAll(paginacao)
+                .map(p -> modelMapper.map(p, PagamentoDTO.class));
+    }
+
+    public PagamentoDTO obterPorId(Long id){
+        Pagamento pagamento = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException());
+
+        return modelMapper.map(pagamento, PagamentoDTO.class);
+    }
+
+    public PagamentoDTO criarPagamento(PagamentoDTO dto){
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDTO.class);
+    }
+
+    public PagamentoDTO atualizarPagamento(Long id, PagamentoDTO dto){
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setId(id);
+        pagamento = repository.save(pagamento);
+        return modelMapper.map(pagamento, PagamentoDTO.class);
+    }
+
+    public void excluirPagamento(Long id){
+        repository.deleteById(id);
+    }
+}
