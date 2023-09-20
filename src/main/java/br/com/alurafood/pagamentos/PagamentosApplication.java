2e4dd2506484029c3603fa4a13b8ec546000893f
Juan Valentin <valentin.juan05@gmail.com>
2023-09-19 22:41:45 -0300

    config eureka server

--- src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -1,9 +1,7 @@
 package br.com.alurafood.pagamentos;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PagamentosApplication {
